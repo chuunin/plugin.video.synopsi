6872d9f79cf0c10bae9e8ac2fe79d95085727367
Tomáš Virgl <tomas@virgl.net>
2012-08-20 22:39:31 +0200

    update

--- plugin.video.synopsi/library.py
+++ plugin.video.synopsi/library.py
@@ -14,11 +14,14 @@ class ApiThread(threading.Thread):
         self.sock.connect(("localhost", 9090))
         # self.sock.connect(("localhost", get_api_port()))
 
+    def process(self, data):
+        pass
+
     def run(self):
-    	global ABORT_REQUESTED    
+        global ABORT_REQUESTED    
         while True:
             data = self.sock.recv(1024)
-            xbmc.log('At {0}: {1}'.format(time.time(), str(data)))
+            # xbmc.log('At {0}: {1}'.format(time.time(), str(data)))
             try:
                 data_json = json.loads(str(data))
                 method = data_json.get("method")
@@ -31,32 +34,99 @@ class ApiThread(threading.Thread):
                 ABORT_REQUESTED = True
                 break
             else:
-                # self.process(data_json)
+                self.process(data_json)
+
+
+class Cache(object):
+    """
+    Library cache.
+    """
+    def __init__(self):
+        super(Cache, self).__init__()
+        self.hash_table = {}
+        self.unhashed = {}
+        self.parsed = True
+
+    def rebuild():
+        """
+        If library cache is broken.
+        """
+        pass
+
+    def get():
+        """
+        Get library cache object.
+        """
+        try:
+            return json.loads(__addon__.getSetting("LIBRARY"))
+        except ValueError, e:
+            return None
+
+    def save():
+        """
+        Save library from memory to file.
+        """
+        global LIBRARY_CACHE
+        __addon__.setSetting(id="LIBRARY", value=LIBRARY_CACHE)
+
+    def add(lib_id, stv_id, hash, lib_type):
+        global LIBRARY_CACHE
+        LIBRARY_CACHE[hash] = {
+            "id" : lib_id,
+            "stvid" : stv_id,
+            "type" : lib_type
+        }
+
+    def get_hash_by_id(_id, _type):
+        return [k for k, v in self.hash_table.iteritems() if v["id"] == _id and v["type"] == _type]
+
+    def get_id_by_hash(_hash):
+        return (self.hash_table[_hash]["id"], self.hash_table[_hash]["type"])
+
+    def get_hash_by_stvid(stvid):
+        return [k for k, v in self.hash_table.iteritems() if v["stvid"] == stvid]
+
+    def get_stvid_by_hash(_hash):
+        return self.hash_table[_hash]["stvid"]
+ 
+    def get_id_by_stvid(stvid):
+        return [(v["id"], v["type"]) for v in self.hash_table.values() if v["stvid"] == stvid]
+
+    def get_stvid_by_id(_id, _type):
+        return [v["stvid"] for v in self.hash_table.values() if v["id"] == _id and v["type"] == _type]
+
+CACHE = Cache()
+
+class Library(ApiThread):
+    """
+    Just a Library.
+    """
+    def __init__(self):
+        super(Library, self).__init__()
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self, _id = None, _type = None, stv_id = None, hash = None):
+        pass
+
+    def process(self, data):
+        if "VideoLibrary" in data['method']:
+            print data
+            if data['method'] == 'VideoLibrary.OnUpdate':
+                pass
+            elif data['method'] == 'VideoLibrary.OnRemove':
                 pass
 
+    # def run(self):
+    #   global ABORT_REQUESTED
 
-class Library(threading.Thread):
-	"""
-	Just a Library.
-	"""
-	def __init__(self):
-		super(Library, self).__init__()
-
-	def create(self):
-		pass
-
-	def read(self):
-		pass
-
-	def update(self):
-		pass
-
-	def delete(self):
-		pass
-
-	def run(self):
-		global ABORT_REQUESTED
-
-		ApiThread().start()
-		while (not ABORT_REQUESTED): # while (not xbmc.abortRequested): # XBMC bug
-			xbmc.sleep(1000)
+    #   ApiThread().start()
+    #   while (not ABORT_REQUESTED): # while (not xbmc.abortRequested): # XBMC bug
+    #       xbmc.sleep(1000)
