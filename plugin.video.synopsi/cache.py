8b67bc5431d4b4fd8836c0d8127f7bccc9db8b25
Tomas Virgl <tomas@virgl.net>
2012-08-28 13:07:37 +0200

    Cache docs

--- plugin.video.synopsi/cache.py
+++ plugin.video.synopsi/cache.py
@@ -13,6 +13,17 @@ def deserialize(_string):
 class Cache(object):
     """
     Library cache.
+    Storing:
+    {
+    "_id" : xbmcid, # not unique
+    "_type": xbmctype, # "movie" or "episode"
+    "_hash": stvhash, # synopsi hash
+    "_file": file, # unique path to ONE file
+    "filepath": filepath # path recieved from xbmc
+    # could be stack:// or stream etc.
+    "imdb": imdb,
+    "stv_id": synopsi_id_library
+    }
     """
     def __init__(self):
         super(Cache, self).__init__()
