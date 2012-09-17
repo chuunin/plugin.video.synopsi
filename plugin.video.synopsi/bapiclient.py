c3342dc838429ed0b8c42982a541077604cc9287
Martin Smid <smid@synopsi.tv>
2012-09-17 15:41:20 +0200

    + basic bapi client

---
+++ plugin.video.synopsi/bapiclient.py
@@ -0,0 +1,125 @@
+import logging
+import json
+import sys
+from base64 import b64encode
+from urllib import urlencode
+from urllib2 import Request, urlopen, HTTPError
+
+
+class BapiClient:
+	def __init__(self, base_url, key, secret, username, password, originReqHost = None, debugLvl = 0):
+		self.baseUrl = base_url
+		self.key = key
+		self.secret = secret
+		self.username = username
+		self.password = password
+		self.accessToken = None
+		self.refreshToken = None
+		self.apiUrl = self.baseUrl + 'bapi/public/1.0/'
+		self.originReqHost = originReqHost or 'dev.bapi.synopsi.tv'
+		self.authHeaders = None
+		self._logger = logging.getLogger()
+		self._logger.addHandler(logging.StreamHandler(sys.stdout))
+
+	def getAccessToken(self):
+		data = {
+			'grant_type': 'password',
+			'client_id': self.key,
+			'client_secret': self.secret,
+			'username': self.username,
+			'password': self.password
+		}
+
+		self.authHeaders = {'AUTHORIZATION': 'BASIC %s' % b64encode("%s:%s" % (self.key, self.secret))}
+
+		# get token
+		try:
+		    response = urlopen(Request(
+		    		base_url + 'oauth2/token/', 
+		    		data=urlencode(data), 
+		    		headers=self.authHeaders, 
+		    		origin_req_host=self.originReqHost))
+
+		    response_json = json.loads(response.readline())
+		except HTTPError as e:
+		    self._logger.error(str(e))
+		    self._logger.error(e.read())
+		    return False
+
+		self.accessToken = response_json['access_token']
+		self.refreshToken = response_json['refresh_token']
+		self._logger.debug('access token = ' + self.accessToken)
+
+	def isAuthorized(self):
+		return self.accessToken != None
+
+	def execute(self, requestData):
+		if not self.isAuthorized():
+			raise Exception('Not Authorized')
+
+		url = self.apiUrl + requestData['methodPath']
+
+		method = requestData['method']
+		
+		if not requestData.has_key('data'):
+			requestData['data'] = {}
+
+		post = {}
+		get = {}
+
+		# append data to post
+		if method == 'post':
+		    post['client_id'] = self.key
+		    post['client_secret'] = self.secret
+		    post['bearer_token'] = self.accessToken
+		    data = urlencode(post)
+		    self._logger.debug(data)
+
+		# append data to get
+		if method == 'get':
+		    get['client_id'] = self.key
+		    get['client_secret'] = self.secret
+		    get['bearer_token'] = self.accessToken
+		    url += '?' + urlencode(get)
+ 		    self._logger.debug(url)
+
+		self._logger.debug('call API')
+		try:
+			    response = urlopen(Request(url,
+		        data = data,
+		        headers = self.authHeaders, 
+		        origin_req_host = self.originReqHost
+		    ))
+		except HTTPError as e:
+		    self._logger.error(str(e))
+		    self._logger.error(e.read())
+		else:
+		    while True:
+		        l = response.readline()
+		        if not l:
+		            break
+		        print l
+
+
+
+#class BapiMethods(BapiClient):
+	def titleWatched(self, titleId):
+		req = {
+			'methodPath': 'title/%d/watched/' % titleId,
+			'method': 'post'
+		}
+
+		self.execute(req)
+
+if __name__ == '__main__':
+	base_url = 'http://neptune.local:8000/'
+	key = 'aafc5cb46d65c6e00fb8082592ce9d'
+	secret = 'fec1f03c3a36fd63978ce6656495ca'
+	username = 'xbmc@synopsi'
+	password = 'aaa'
+
+	client = BapiClient(base_url, key, secret, username, password)
+	client.getAccessToken()
+	client.titleWatched(2848299)
+
+
