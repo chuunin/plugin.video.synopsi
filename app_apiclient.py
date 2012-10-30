bb4ea3b739109015fac39aef58938f9e7a83fa37
Martin Smid <smid@synopsi.tv>
2012-10-30 11:38:58 +0100

    + orig. dialog instead of custom, use openSettings + remember reject

--- app_apiclient.py
+++ app_apiclient.py
@@ -1,11 +1,13 @@
 from apiclient import *
 from utilities import *
 import threading
+import xbmcgui
 
 class AppApiClient(ApiClient):
 	def __init__(self, base_url, key, secret, username, password, device_id, originReqHost=None, debugLvl=logging.INFO, accessTokenTimeout=10, rel_api_url='api/public/1.0/'):
 		super(AppApiClient, self).__init__(base_url, key, secret, username, password, device_id, originReqHost, debugLvl, accessTokenTimeout, rel_api_url)
 		self._lock_access_token = threading.Lock()
+		self._rejected_to_correct = False
 
 	def getAccessToken(self):
 		if not self._lock_access_token.acquire(False):
@@ -19,9 +21,20 @@ class AppApiClient(ApiClient):
 			try:
 				ApiClient.getAccessToken(self)
 			# in failure, ask for new login/pass and repeat if dialog was not canceled
+				# raise AuthenticationError
 			except AuthenticationError:
 				# crashes
 				# finished = not login_screen(self)
+				if self._rejected_to_correct:
+					notification('Authentication failed. Correct your login/password in plugin settings')
+				else:
+					if xbmcgui.Dialog().yesno("SynopsiTV", "Authentication failed", "Would you like to open settings and correct your login info?"):
+						addon = xbmcaddon.Addon()
+						addon.openSettings()
+						self.setUserPass(addon.getSetting('USER'), addon.getSetting('PASS'))
+					else:
+						self._rejected_to_correct = True
+
 				finished = True
 				xbmc.log('Trying new credentials ? %d' % int(not finished))
 			except Exception as e:
