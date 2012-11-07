8bcf740ace443aaf148919ff9f79983c705f2cb7
Martin Smid <smid@synopsi.tv>
2012-11-07 15:42:14 +0100

    + unittest for cacheapiclient

---
+++ tests/cachedapiclient.unittest.py
@@ -0,0 +1,98 @@
+import os, sys
+import unittest
+import logging
+import json
+from copy import copy
+
+sys.path.insert(0, os.path.abspath('..'))
+from cachedapiclient import *
+
+def pprint(data):
+	global logger
+
+	if data is dict and data.has_key('_db_queries'):
+		del data['_db_queries']
+	msg = json.dumps(data, indent=4)
+	# print msg
+	logger.debug(msg)
+
+
+class ApiTest(unittest.TestCase):
+	def test_auth(self):
+		global connection
+
+		c = connection		
+		client = ApiClient(c['base_url'], c['key'], c['secret'], c['username'], c['password'], c['device_id'], debugLvl = logging.WARNING, rel_api_url=c['rel_api_url'])
+		client.getAccessToken()
+		self.assertIsInstance(client, ApiClient)
+		return client
+
+	def test_library_add(self):
+		global connection
+
+		c = connection		
+		client = ApiClient(c['base_url'], c['key'], c['secret'], c['username'], c['password'], c['device_id'], debugLvl=logging.WARNING, rel_api_url=c['rel_api_url'])
+		client.getAccessToken()
+
+		# 60569 "Malcolm X"
+		data = client.titleIdentify(imdb_id=60569, stv_title_hash='')
+
+		stv_title_id = data['title_id']
+
+		data = client.libraryTitleAdd(stv_title_id)
+
+		exampleEvents = [
+			{
+			    "event": "start", 
+			    "timestamp": '2012-10-08 16:54:34', 
+			    "position": 1222
+			}, 
+			{
+			    "event": "pause", 
+			    "timestamp": '2012-10-08 16:54:40', 
+			    "position": 1359
+			}, 
+			{
+			    "event": "resume", 
+			    "timestamp": '2012-10-08 16:55:10', 
+			    "position": 1359
+			}, 
+			{
+			    "event": "stop", 
+			    "timestamp": '2012-10-08 16:55:15', 
+			    "position": 1460
+			},
+		]
+
+		#exampleEvents = []
+
+		watched_data = {
+			'rating': 1, 
+			'player_events': json.dumps(exampleEvents)
+		}
+
+		data = client.titleWatched(stv_title_id, **watched_data)
+		data = client.libraryTitleRemove(stv_title_id)
+
+
+if __name__ == '__main__': 
+	connection = {
+		'base_url': 'http://test.papi.synopsi.tv/',
+		'key': 'e20e8f465f42e96d8340e81bfc48c757',
+		'secret': '72ab379c50650f7aec044b14db306430a55f09a2da1eb8e40b297a54b30e4b4f',
+		'rel_api_url': '1.0/',
+		# 'base_url': 'http://neptune.local:8000/',
+		# 'key': '76ccb5ec8ecddf15c29c5decac35f9',
+		# 'secret': '261029dbbdd5dd481da6564fa1054e',
+		# 'rel_api_url': 'api/public/1.0/',
+		'username': 'martin.smid@gmail.com',
+		'password': 'aaa',
+		'device_id': '7caa970e-0e37-11e2-9462-7cc3a1719bfd',
+	}
+
+	logger = logging.getLogger()
+
+	suite = unittest.TestLoader().loadTestsFromTestCase(ApiTest)
+	unittest.TextTestRunner(verbosity=2).run(suite)
+
+
