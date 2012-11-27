81ea5d962558f6fd2c4cae6aab6e951c461aeff6
Martin Smid <martin.smid@gmail.com>
2012-11-27 18:35:57 +0100

    * indent using tabs - useless tests

--- library.py
+++ library.py
@@ -1,7 +1,4 @@
-try:
-    import xbmc, xbmcgui, xbmcaddon
-except ImportError:
-    from tests import xbmc, xbmcgui, xbmcaddon
+import xbmc, xbmcgui, xbmcaddon
 import threading
 import time
 import socket
@@ -17,132 +14,132 @@ ABORT_REQUESTED = False
 
 
 class RPCListener(threading.Thread):
-    def __init__(self, cache, scrobbler):
-        super(RPCListener, self).__init__()
-        self.cache = cache
-        self.scrobbler = scrobbler
-        self.apiclient = None
-        self.sock = socket.socket()
-        self.sock.settimeout(5)
-        self.connected = False
-        sleepTime = 100
-        t = time.time()
-        while sleepTime<500 and (not self.connected or ABORT_REQUESTED or xbmc.abortRequested):
-            try:
-                self.sock.connect(("localhost", 9090))  #   TODO: non default api port (get_api_port)
-            except Exception, exc:
-                log('%0.2f %s' % (time.time() - t, str(exc)))
-                xbmc.sleep(int(sleepTime))
-                sleepTime *= 1.5
-            else:
-                log('Connected to 9090')
-                self.connected = True
+	def __init__(self, cache, scrobbler):
+		super(RPCListener, self).__init__()
+		self.cache = cache
+		self.scrobbler = scrobbler
+		self.apiclient = None
+		self.sock = socket.socket()
+		self.sock.settimeout(5)
+		self.connected = False
+		sleepTime = 100
+		t = time.time()
+		while sleepTime<500 and (not self.connected or ABORT_REQUESTED or xbmc.abortRequested):
+			try:
+				self.sock.connect(("localhost", 9090))  #   TODO: non default api port (get_api_port)
+			except Exception, exc:
+				log('%0.2f %s' % (time.time() - t, str(exc)))
+				xbmc.sleep(int(sleepTime))
+				sleepTime *= 1.5
+			else:
+				log('Connected to 9090')
+				self.connected = True
 
-        self.sock.setblocking(True)
+		self.sock.setblocking(True)
 
-    def process(self, data):
-        pass
+	def process(self, data):
+		pass
 
-    def run(self):
-        global ABORT_REQUESTED
+	def run(self):
+		global ABORT_REQUESTED
 
-        if not self.connected:
-            log('RPC Listener cannot run, there is not connection to xbmc')
-            return False
+		if not self.connected:
+			log('RPC Listener cannot run, there is not connection to xbmc')
+			return False
 
-        self.apiclient = AppApiClient.getDefaultClient()
+		self.apiclient = AppApiClient.getDefaultClient()
 
-        while True:
-            data = self.sock.recv(8192)
-            data = data.replace('}{', '},{')
-            datapack='[%s]' % data
-            # log('SynopsiTV: {0}'.format(str(data)))
-            try:
-                data_json = json.loads(datapack)
-            except ValueError, e:
-                log('RPC ERROR:' + str(e))
-                log('RPC ERROR DATA:' + str(data))
-                continue
+		while True:
+			data = self.sock.recv(8192)
+			data = data.replace('}{', '},{')
+			datapack='[%s]' % data
+			# log('SynopsiTV: {0}'.format(str(data)))
+			try:
+				data_json = json.loads(datapack)
+			except ValueError, e:
+				log('RPC ERROR:' + str(e))
+				log('RPC ERROR DATA:' + str(data))
+				continue
 
-            for request in data_json:
-                method = request.get("method")
+			for request in data_json:
+				method = request.get("method")
 
-                if method == "System.OnQuit":
-                    ABORT_REQUESTED = True
-                    break
-                else:
-                    self.process(request)
+				if method == "System.OnQuit":
+					ABORT_REQUESTED = True
+					break
+				else:
+					self.process(request)
 
-        self.sock.close()
-        log('Library thread end')
+		self.sock.close()
+		log('Library thread end')
 
-    def process(self, data):
-        methodName = data['method'].replace('.', '_')
-        method = getattr(self, methodName, None)
-        if method == None:
-            log('Unknown method: ' + methodName)
-            return
+	def process(self, data):
+		methodName = data['method'].replace('.', '_')
+		method = getattr(self, methodName, None)
+		if method == None:
+			log('Unknown method: ' + methodName)
+			return
 
-        log(str(data))
-        
-        #   Try to call that method
-        try:
-            method(data)
-        except:
-            log('Error in method "' + methodName + '"')
-            log(traceback.format_exc())
+		log(str(data))
+		
+		#   Try to call that method
+		try:
+			method(data)
+		except:
+			log('Error in method "' + methodName + '"')
+			log(traceback.format_exc())
 
-        #   http://wiki.xbmc.org/index.php?title=JSON-RPC_API/v4
+		#   http://wiki.xbmc.org/index.php?title=JSON-RPC_API/v4
 
 
 class RPCListenerHandler(RPCListener):
-    """
-    RPCListenerHandler defines event handler methods that are autotically called from parent class's RPCListener
-    """
-    def __init__(self, cache, scrobbler):
-        super(RPCListenerHandler, self).__init__(cache, scrobbler)
+	"""
+	RPCListenerHandler defines event handler methods that are autotically called from parent class's RPCListener
+	"""
+	def __init__(self, cache, scrobbler):
+		super(RPCListenerHandler, self).__init__(cache, scrobbler)
 
 	def _xbmc_time2sec(self, time):
 		return time["hours"] * 3600 + time["minutes"] * 60 + time["seconds"] + time["milliseconds"] / 1000
 		
-    #   NOT USED NOW
-    def playerEvent(self, data):
-        self.log(dump(data))
+	#   NOT USED NOW
+	def playerEvent(self, data):
+		self.log(dump(data))
 
-    def log(self, msg):
-        log('LIBRARY / ' + msg)
+	def log(self, msg):
+		log('LIBRARY / ' + msg)
 
-    def VideoLibrary_OnUpdate(self, data):
-        i = data['params']['data']['item']
-        self.cache.addorupdate(i['type'], i['id'])
+	def VideoLibrary_OnUpdate(self, data):
+		i = data['params']['data']['item']
+		self.cache.addorupdate(i['type'], i['id'])
 
-    def VideoLibrary_OnRemove(self, data):
-        d = data['params']['data']
-        self.cache.remove(d['type'], d['id'])
+	def VideoLibrary_OnRemove(self, data):
+		d = data['params']['data']
+		self.cache.remove(d['type'], d['id'])
 
-    def Player_OnPlay(self, data):
-        self.playerEvent(data)
+	def Player_OnPlay(self, data):
+		self.playerEvent(data)
 
-    def Player_OnStop(self, data):
-        self.playerEvent(data)
+	def Player_OnStop(self, data):
+		self.playerEvent(data)
 
-    def Player_OnSeek(self, data):
+	def Player_OnSeek(self, data):
 		position = self._xbmc_time2sec(data['params']['data']['player']['time'])
 		self.scrobbler.playerEventSeek(position)
 
-    def Player_OnPause(self, data):
-        self.playerEvent(data)
-        pass
+	def Player_OnPause(self, data):
+		self.playerEvent(data)
+		pass
 
-    def Player_OnResume(self, data):
-        self.playerEvent(data)
-        pass
+	def Player_OnResume(self, data):
+		self.playerEvent(data)
+		pass
 
-    def GUI_OnScreenSaverActivated(self, data):
-        self.playerEvent(data)
-        pass
+	def GUI_OnScreenSaverActivated(self, data):
+		self.playerEvent(data)
+		pass
 
-    def GUI_OnScreenSaverDeactivated(self, data):
-        self.playerEvent(data)
-        pass
+	def GUI_OnScreenSaverDeactivated(self, data):
+		self.playerEvent(data)
+		pass
 
--- tests/xbmc.py
+++ tests/xbmc.py
@@ -10,64 +10,64 @@ import threading
 abortRequested = False
 
 def sleep(tim):
-    pass
+	pass
 
 
 def log(string):
-    print string
+	print string
 
 
 def executeJSONRPC(js):
-    print "XBMC recieved: ", js.strip()
-    return "{}"
+	print "XBMC recieved: ", js.strip()
+	return "{}"
 
 
 def executebuiltin(builtin):
-    if "XBMC.Notification" in builtin:
-        pass
-        a = builtin.split(",")
-        print "XBMC message:", a[0].strip("XBMC.Notification("), a[1]
-    else:
-        print "Executed Builtin:", builtin
+	if "XBMC.Notification" in builtin:
+		pass
+		a = builtin.split(",")
+		print "XBMC message:", a[0].strip("XBMC.Notification("), a[1]
+	else:
+		print "Executed Builtin:", builtin
 
 
 class Player(threading.Thread):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.start()
+	def __init__(self):
+		super(Player, self).__init__()
+		self.start()
 
-    def onPlayBackStarted(self):
-        pass
+	def onPlayBackStarted(self):
+		pass
 
-    def onPlayBackEnded(self):
-        pass
+	def onPlayBackEnded(self):
+		pass
 
-    def onPlayBackStopped(self):
-        pass
+	def onPlayBackStopped(self):
+		pass
 
-    def onPlayBackPaused(self):
-        pass
+	def onPlayBackPaused(self):
+		pass
 
-    def onPlayBackResumed(self):
-        pass
+	def onPlayBackResumed(self):
+		pass
 
-    def isPlayingVideo(self):
-        return True
+	def isPlayingVideo(self):
+		return True
 
-    def getTime(self):
-        return 71
+	def getTime(self):
+		return 71
 
-    def getTotalTime(self):
-        return 100
+	def getTotalTime(self):
+		return 100
 
-    def getPlayingFile(self):
-        return ""
+	def getPlayingFile(self):
+		return ""
 
-    def run(self):
-        self.onPlayBackStarted()
-        time.sleep(0.21)
-        self.onPlayBackResumed()
-        time.sleep(0.21)
-        self.onPlayBackPaused()
-        time.sleep(0.21)
-        self.onPlayBackStopped()
+	def run(self):
+		self.onPlayBackStarted()
+		time.sleep(0.21)
+		self.onPlayBackResumed()
+		time.sleep(0.21)
+		self.onPlayBackPaused()
+		time.sleep(0.21)
+		self.onPlayBackStopped()
