6a4bccc50c79930be3a78297ebfedacdb16dcf33
Martin Smid <martin.smid@gmail.com>
2012-12-20 17:35:36 +0100

    * own listing windows/dialogs [in-progress, not working at all !!@#$@!#!@$]

---
+++ dialog.py
@@ -0,0 +1,54 @@
+
+# xbmc
+import xbmcgui
+
+# application
+from utilities import *
+
+
+__addon__  = get_current_addon()
+__cwd__	= __addon__.getAddonInfo('path')
+
+
+class ListDialog(xbmcgui.WindowXMLDialog):
+	""" Dialog for choosing movie corrections """
+	def __init__(self, *args, **kwargs):
+		self.data = kwargs['data']
+		self.controlId = None
+		self.selectedMovie = None
+
+	def onInit(self):
+		items = []
+		for item in self.data['items']:
+			li = xbmcgui.ListItem(item['name'], iconImage=item['cover_medium'])
+			li.setProperty('id', str(item['id']))
+			li.setProperty('CustomOverlay', 'already-watched-stack.png')
+			items.append(li)
+
+		xbmc.executebuiltin("Container.SetViewMode(500)")
+
+		self.getControl(500).addItems(items)
+
+	def onFocus(self, controlId):
+		self.controlId = controlId
+
+	def onAction(self, action):
+		log('action: %s focused id: %s' % (str(action.getId()), str(self.controlId)))
+
+
+def open_list_dialog(tpl_data):
+	print 'cwd: ' + __cwd__
+	#~ path = '/home/smid/projects/XBMC/resources/skins/Default/720p/'
+	path = ''
+	try:
+		win = xbmcgui.Window(xbmcgui.getCurrentWindowDialogId())
+	except ValueError, e:
+		ui = ListDialog(path + "custom_MyVideoNav.xml", __cwd__, "Default", data=tpl_data)
+		ui.doModal()
+		del ui
+	else:
+		win = xbmcgui.WindowDialog(xbmcgui.getCurrentWindowDialogId())
+		win.close()
+		ui = ListDialog(path + "custom_MyVideoNav.xml", __cwd__, "Default", data=tpl_data)
+		ui.doModal()
+		del ui
