c52e170d28634cf04759454cad88c0dcf4cda3f8
Martin Smid <martin.smid@gmail.com>
2012-12-28 12:49:02 +0100

    * cherry-pick, update key/secret in tests
    Conflicts:
    
    	utilities.py

--- tests/common.py
+++ tests/common.py
@@ -1,15 +1,15 @@
 
 connection = {
-	'base_url': 'https://test-papi.synopsi.tv/',
-	'key': 'e20e8f465f42e96d8340e81bfc48c757',
-	'secret': '72ab379c50650f7aec044b14db306430a55f09a2da1eb8e40b297a54b30e4b4f',
+	'base_url': 'https://api.synopsi.tv/',
+	'key': '59c53964b1013defcff0155f6e4d54a4',
+	'secret': '487615d20b22cdce510fd3476ed84d924e2b0c45ce7c49dc621764e05fae0904',
 	'rel_api_url': '1.0/',
 	# 'base_url': 'http://neptune.local:8000/',
 	# 'key': '76ccb5ec8ecddf15c29c5decac35f9',
 	# 'secret': '261029dbbdd5dd481da6564fa1054e',
 	# 'rel_api_url': 'api/public/1.0/',
-	'username': 'martin.smid@gmail.com',
-	'password': 'aaa',
+	'username': '',
+	'password': '',
 	'device_id': 'd2a8e78f-408a-11e2-9b7d-9927c8ae50bb',
 }
 
