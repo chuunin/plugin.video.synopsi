9a82825ff244aebf968f895ad73cc3c97746096d
Martin Smid <martin.smid@gmail.com>
2013-01-04 16:19:34 +0100

    - unused param
    Conflicts:
    
    	addon.py

--- addon.py
+++ addon.py
@@ -188,15 +188,12 @@ try:
 
 	addonclient = AddonClient(dirhandle)
 
-	param_vars = ['url', 'name', 'mode', 'type', 'data', 'stv_id']
+	param_vars = ['url', 'mode', 'type', 'data', 'stv_id']
 	p = dict([(k, params.get(k, [None])[0]) for k in param_vars])
 
 	try:
 		if p['url']:
 			p['url']=uniunquote(p['url'])
-
-		if p['name']:
-			p['name']=uniunquote(p['name'])
 
 		if p['mode']:
 			p['mode']=int(p['mode'])
