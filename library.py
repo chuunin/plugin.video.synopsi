99f59ff556b2ebdf05bf702525345b2e4d4b134b
Martin Šmid <martin.smid@gmail.com>
2013-01-23 13:12:48 +0100

    *	remove scrobbler, move xbmc.player to main (service) thread *	rpc listener - remove scrobbler dependency, use global player
    Conflicts:
    	_src/TODO
    	scrobbler.py

--- library.py
+++ library.py
@@ -13,11 +13,10 @@ ABORT_REQUESTED = False
 
 
 class RPCListener(MyThread):
-	def __init__(self, cache, scrobbler):
+	def __init__(self, cache):
 		super(RPCListener, self).__init__()
 
 		self.cache = cache
-		self.scrobbler = scrobbler
 		self.sock = socket.socket()
 		self.sock.settimeout(5)
 		self.connected = False
@@ -96,8 +95,8 @@ class RPCListenerHandler(RPCListener):
 	"""
 	RPCListenerHandler defines event handler methods that are autotically called from parent class's RPCListener
 	"""
-	def __init__(self, cache, scrobbler):
-		super(RPCListenerHandler, self).__init__(cache, scrobbler)
+	def __init__(self, cache):
+		super(RPCListenerHandler, self).__init__(cache)
 
 	def _xbmc_time2sec(self, time):
 		return time["hours"] * 3600 + time["minutes"] * 60 + time["seconds"] + time["milliseconds"] / 1000
@@ -122,7 +121,7 @@ class RPCListenerHandler(RPCListener):
 
 	def Player_OnSeek(self, data):
 		position = self._xbmc_time2sec(data['params']['data']['player']['time'])
-		self.scrobbler.player.playerEventSeek(position)
+		top.player.playerEventSeek(position)
 
 	def Player_OnPause(self, data):
 		self.playerEvent(data)
