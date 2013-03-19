798ec779cde9b8a02e7895729669ad3ccf5eeb09
Martin Šmid <martin.smid@gmail.com>
2013-03-19 11:16:35 +0100

    * use xbmcvfs where possible

--- utilities.py
+++ utilities.py
@@ -1,5 +1,5 @@
 # xbmc
-import xbmc, xbmcgui, xbmcaddon, xbmcplugin
+import xbmc, xbmcgui, xbmcaddon, xbmcplugin, xbmcvfs
 import CommonFunctions
 
 # python standart lib
@@ -412,17 +412,36 @@ def is_protected(path):
 	return False
 
 
+def textfilter(bytestring):
+	import string,re
+
+	norm = string.maketrans('', '') #builds list of all characters
+	non_alnum = string.translate(norm, norm, string.letters+string.digits) 
+	
+	trans_nontext=string.maketrans(non_alnum,'?'*len(non_alnum))
+	cleaned=string.translate(bytestring, trans_nontext)
+	
+	return cleaned
+
 def stv_hash(filepath):
 	"""
 	New synopsi hash. Hashing the sedond 512 kB of a file using SHA1.
 	"""
 
+	chunk_offset = 524288
+	chunk_length = 524288
+	
 	sha1 = hashlib.sha1()
 
 	try:
-		with open(filepath, 'rb') as f:
-			f.seek(524288, 0)
-			sha1.update(f.read(524288))
+		f = xbmcvfs.File(filepath, 'r')
+		f.seek(chunk_offset, 0)
+		fcontent = f.read(chunk_length)
+		if len(fcontent) != chunk_length:
+			raise IOError()
+			
+		sha1.update(fcontent)
+		f.close()
 	except (IOError) as e:
 		raise HashError('Unable to hash file [%s]' % filepath)
 
@@ -437,10 +456,11 @@ def old_stv_hash(filepath):
 	sha1 = hashlib.sha1()
 
 	try:
-		with open(filepath, 'rb') as f:
-			sha1.update(f.read(256))
-			f.seek(-256, 2)
-			sha1.update(f.read(256))
+		f = xbmcvfs.File(filepath, 'rb')
+		sha1.update(f.read(256))
+		f.seek(-256, 2)
+		sha1.update(f.read(256))
+		f.close()
 	except (IOError) as e:
 		return None
 
@@ -455,9 +475,9 @@ def hash_opensubtitle(name):
 		longlongformat = 'q'  # long long
 		bytesize = struct.calcsize(longlongformat)
 
-		_file = open(name, "rb")
+		_file = xbmcvfs.File(name, 'rb')
 
-		filesize = os.path.getsize(name)
+		filesize = _file.size()
 		hash = filesize
 
 		if filesize < 65536 * 2:
