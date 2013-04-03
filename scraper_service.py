5bdd56180c03633b81163700e8a989186c8153cf
Martin Šmid <martin.smid@gmail.com>
2013-04-03 18:12:37 +0200

    + scraper server, handler and thread classes and basic methods

--- scraper_service.py
+++ scraper_service.py
@@ -1,9 +1,14 @@
 
 
 # python standart lib
-import sys, json, urlparse
+import sys
+import json
+import urlparse
+import traceback
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
+from threading import Thread
+from httplib2 import Http
 
 
 class ScraperServer(BaseHTTPServer.HTTPServer):
@@ -15,27 +20,56 @@ class ScraperRequestHandler(SimpleHTTPRequestHandler):
 
 
 	def do_GET(s):
-		url = urlparse.urlparse(s.path)
-		qs = urlparse.parse_qs(url.query)
-		print qs
-		
-		s.send_response(200)
-		s.send_header("Content-type", "text/json")
-		s.end_headers()
+		try:
+			url = urlparse.urlparse(s.path)
+			qs = urlparse.parse_qs(url.query)
+			file_name = qs['q'][0]
 
-		response = {}
-		response['container'] = 'garbage'
+			s.send_response(200)
+			s.send_header("Content-type", "text/json")
+			s.end_headers()
+
+			response = {}
+			response['requested'] = file_name
+		except:
+			exc_type, exc_value, exc_traceback = sys.exc_info()
+			response = {'status': 'exception', 'traceback': traceback.extract_tb(exc_traceback)}
 
 		s.wfile.write(json.dumps(response))
+		s.wfile.write('\n')
+
+class ScraperServerThread(ScraperServer, Thread):
+	_stopped = False
+
+	def __init__(self, server_address):
+		ScraperServer.__init__(self, server_address)
+		Thread.__init__(self)
+
+	def serve_forever(self):
+		while not self._stopped:
+			self.handle_request()
+
+	def run(self):
+		self.serve_forever()
+
+	def server_stop(self):
+		self._stopped = True
+		self.create_dummy_request()
+		self.server_close()
+
+	def create_dummy_request(self):
+		h = Http()
+		h.request('http://%s:%s' % self.server_address, "HEAD")
 
 if __name__ == '__main__':
 	server_address = ('127.0.0.1', 9099)
-	httpd = ScraperServer(server_address)
-	sa = httpd.socket.getsockname()
-	print "Serving HTTP on", sa[0], "port", sa[1], "..."
+	scraper_server_thread = ScraperServerThread(server_address)
+	scraper_server_thread.start()
 
+	print 'Scraper Server is running'
+	# scraper_server_thread.join(0.2)
 	try:
-		httpd.serve_forever()
+		while 1:
+			pass
 	except KeyboardInterrupt:
-		pass
-	httpd.server_close()
+		scraper_server_thread.server_stop()
